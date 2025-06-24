ad6fcb32edb276d00481c67b2d7427e921004a16
Adrian Wowk <dev@adrianwowk.com>
2025-06-24 11:56:05 -0500

    docs: add todos

--- src/middle/optimization/ssa/mod.rs
+++ src/middle/optimization/ssa/mod.rs
@@ -1,4 +1,8 @@
 // TODO: remove redundant get_element_pointer computations for the same
 // structure
+
 // TODO: remove alloc instructions where all the fields are immediately
-// deconstructed and the ptr is not used for any other purpose
+// deconstructed and the ptr is not used for any other purpose. to do this,
+// check that the pointer returned from alloc is only used in get_element_ptr
+// and the returned pointers are only used for load/store instructions. if thats
+// the case, just replace the load dest regs with the initial values.
